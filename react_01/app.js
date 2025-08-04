8d2188c12f7c44d39942548b415c82027ca9dd44
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-08-04 12:16:49 +0530

    Chapter-01

---
+++ react_01/app.js
@@ -0,0 +1,13 @@
+const heading = React.createElement(
+  "div",
+  { id: "Parents" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement(["h1", {}, "Hello React.js, Durgesh!"])
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
