71687fa51a4abbc2ac1d820f0a9d0f9f9eefea1b
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-08-05 17:43:02 +0530

    Add scripts to run the code. Learn about functional component

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "div",
-  { id: "Parents" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "Hello React.js, Durgesh!")
-  )
+const Title = () => {
+  return <h1 id="heading"> Hii, Durgesh Shekhawat</h1>;
+};
+
+
+//Component Compositions
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <NameD />
+    <h1>Welcome!! to the React World</h1>
+  </div>
 );
+
+const NameD = () => <h1>Hii, Saumya</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
